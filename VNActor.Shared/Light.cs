ece15d15afdcf5a89d95486986131ac0d08feb2e
Hallongrotta <globboglabbi@gmail.com>
2020-08-24 11:04:43 +0200

    unify light code

--- VNActor.Shared/Light.cs
+++ VNActor.Shared/Light.cs
@@ -1,10 +1,49 @@
-﻿using Studio;
+﻿using MessagePack;
+using Studio;
 using UnityEngine;
 
 namespace VNActor
 {
     public partial class Light : HSNeoOCIProp, IVNObject
     {
+
+        [MessagePackObject]
+        public class LightData : IDataClass
+        {
+            [Key(0)]
+            public Color color;
+            [Key(1)]
+            public bool enable;
+            [Key(2)]
+            public float intensity;
+            [Key(3)]
+            public bool shadow;
+            [Key(4)]
+            public float range;
+            [Key(5)]
+            public float angle;
+            [Key(6)]
+            public Vector3 position;
+            [Key(7)]
+            public Vector3 rotation;
+
+            public LightData()
+            {
+
+            }
+
+            public LightData(Light l)
+            {
+                position = l.pos;
+                rotation = l.rot;
+                color = l.color;
+                enable = l.enable;
+                intensity = l.intensity;
+                shadow = l.shadow;
+                range = l.range;
+                angle = l.angle;
+            }
+        }
 
         new protected OCILight objctrl;
 
@@ -34,6 +73,67 @@ namespace VNActor
             Vector3 rt = this.objctrl.objectInfo.changeAmount.rot;
             Vector3 nrt = new Vector3(rt.x + param[0], rt.y + param[1], rt.z + param[2]);
             this.objctrl.objectInfo.changeAmount.rot = nrt;
+        }
+
+        public float range
+        {
+            get
+            {
+                return objctrl.lightInfo.range;
+            }
+            set
+            {
+                objctrl.SetRange(value);
+            }
+        }
+
+        public bool shadow
+        {
+            get
+            {
+                return objctrl.lightInfo.shadow;
+            }
+            set
+            {
+                objctrl.SetShadow(value);
+            }
+        }
+
+        public float angle
+        {
+            get
+            {
+                return objctrl.lightInfo.spotAngle;
+            }
+            set
+            {
+                objctrl.SetSpotAngle(value);
+            }
+        }
+
+        public Color color
+        {
+            get
+            {
+                return objctrl.lightInfo.color;
+            }
+            set
+            {
+                objctrl.SetColor(value);
+            }
+        }
+
+        public float intensity
+        {
+            get
+            {
+
+                return objctrl.lightInfo.intensity;
+            }
+            set
+            {
+                objctrl.SetIntensity(value);
+            }
         }
 
         public bool enable
@@ -112,6 +212,31 @@ namespace VNActor
         public static void prop_range(Light prop, LightData param)
         {
             prop.range = param.range;
+        }
+
+        override public IDataClass export_full_status()
+        {
+            return new LightData(this);
+        }
+
+        public void import_status(LightData l)
+        {
+            pos = l.position;
+            rot = l.rotation;
+            color = l.color;
+            enable = l.enable;
+            intensity = l.intensity;
+            shadow = l.shadow;
+            range = l.range;
+            angle = l.angle;
+        }
+
+        override public void import_status(IDataClass status)
+        {
+            if (status is LightData)
+            {
+                import_status(status);
+            }
         }
     }
 }
